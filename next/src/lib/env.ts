5688ec1eef1156cb546641d6c250aae33486aa53
MichaelHelgesen <michaelhelgesen02@gmail.com>
2025-08-11 15:34:48 +0200

    Changed ENV-variables

--- next/src/lib/env.ts
+++ next/src/lib/env.ts
@@ -1,5 +1,15 @@
 export const SNIPCART_API_KEY = process.env.NEXT_PUBLIC_SNIPCART_API_KEY;
-
 if(!SNIPCART_API_KEY) {
     throw new Error("Miljøvariabel NEXT_PUBLIC_SNIPCART_API_KEY mangler");
+}
+
+export const SANITY_API_KEY = process.env.NEXT_PUBLIC_SANITY_API_KEY;
+if(!SANITY_API_KEY) {
+    throw new Error("Miljøvariabel NEXT_PUBLIC_SANITY_API_KEY mangler");
+}
+
+
+export const SANITY_DATASET = process.env.NEXT_PUBLIC_SANITY_DATASET;
+if(!SANITY_DATASET) {
+    throw new Error("Miljøvariabel NEXT_PUBLIC_SANITY_DATASET mangler");
 }
--- next/src/lib/sanity.ts
+++ next/src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client"
-
+import { SANITY_DATASET, SANITY_API_KEY} from "@/lib/env"
 export const sanityClient = createClient ({
-    projectId: process.env.SANITY_API_TOKEN,
-    dataset: "production",
+    projectId: SANITY_API_KEY as string,
+    dataset: SANITY_DATASET as string,
     useCdn: true,
     apiVersion: "2023-01-01",
 })
